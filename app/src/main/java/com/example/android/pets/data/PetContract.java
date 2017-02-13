079c9e801b37d45fcdc1a03bb2f23db5b2101bcc
linucksrox <ericdalyblog@dalydays.com>
2017-02-13 07:24:32 -0500

    Add gender constants and use in EditorActivity

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -20,6 +20,10 @@ public final class PetContract {
         public static final String COLUMN_PET_BREED = "breed";
         public static final String COLUMN_PET_GENDER = "gender";
         public static final String COLUMN_PET_WEIGHT = "weight";
+
+        public static final int GENDER_UNKNOWN = 0;
+        public static final int GENDER_MALE = 1;
+        public static final int GENDER_FEMALE = 2;
     }
 
 }
