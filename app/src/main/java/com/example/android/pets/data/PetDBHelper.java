933ffe623ae1f32cd0f00cf7a6d160068f7235f4
Eric Daly <ericdalyblog@dalydays.com>
2017-02-13 14:54:59 -0500

    Add PetDbHelper and create the legit database fo real

---
+++ app/src/main/java/com/example/android/pets/data/PetDBHelper.java
@@ -0,0 +1,37 @@
+package com.example.android.pets.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.android.pets.data.PetContract.PetEntry;
+
+/**
+ * Created by edaly on 2/13/2017.
+ */
+
+public class PetDbHelper extends SQLiteOpenHelper {
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "pets.db";
+
+    public PetDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String SQL_CREATE_ENTRIES = "CREATE TABLE " + PetEntry.TABLE_NAME + "("
+                + PetEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + PetEntry.COLUMN_PET_NAME + " TEXT NOT NULL, "
+                + PetEntry.COLUMN_PET_BREED + " TEXT, "
+                + PetEntry.COLUMN_PET_GENDER + " INTEGER NOT NULL, "
+                + PetEntry.COLUMN_PET_WEIGHT + " INTEGER NOT NULL DEFAULT 0);";
+        sqLiteDatabase.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        //
+    }
+}
