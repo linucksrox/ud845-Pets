0d49a0e7384f937797e16560b74e757455d975cb
linucksrox <ericdalyblog@dalydays.com>
2017-02-13 07:18:33 -0500

    Add new pet contract

---
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -0,0 +1,25 @@
+package com.example.android.pets.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by edaly on 2/13/17.
+ */
+
+public final class PetContract {
+
+    private PetContract() {
+    }
+
+    public static final class PetEntry implements BaseColumns {
+
+        public final static String TABLE_NAME = "pets";
+
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_PET_NAME = "name";
+        public static final String COLUMN_PET_BREED = "breed";
+        public static final String COLUMN_PET_GENDER = "gender";
+        public static final String COLUMN_PET_WEIGHT = "weight";
+    }
+
+}
