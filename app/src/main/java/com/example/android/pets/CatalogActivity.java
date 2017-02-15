5474429d59803b6494eb565ba82795a5dad7bd0d
Eric Daly <ericdalyblog@dalydays.com>
2017-02-15 15:18:00 -0500

    List all table info in CatalogActivity

--- app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -61,12 +61,36 @@ public class CatalogActivity extends AppCompatActivity {
         // Create and/or open a database to read from it
         SQLiteDatabase db = mDbHelper.getReadableDatabase();
 
+        TextView displayView = (TextView) findViewById(R.id.text_view_pet);
+
         Cursor cursor = db.query(PetEntry.TABLE_NAME, null, null, null, null, null, null);
+        int petID = cursor.getColumnIndex(PetEntry._ID);
+        int petName = cursor.getColumnIndex(PetEntry.COLUMN_PET_NAME);
+        int petBreed = cursor.getColumnIndex(PetEntry.COLUMN_PET_BREED);
+        int petGender = cursor.getColumnIndex(PetEntry.COLUMN_PET_GENDER);
+        int petWeight = cursor.getColumnIndex(PetEntry.COLUMN_PET_WEIGHT);
         try {
             // Display the number of rows in the Cursor (which reflects the number of rows in the
             // pets table in the database).
-            TextView displayView = (TextView) findViewById(R.id.text_view_pet);
-            displayView.setText("Number of rows in pets database table: " + cursor.getCount());
+            displayView.setText("The pets table contains " + cursor.getCount() + " pets.");
+            displayView.append("\n");
+            displayView.append("\n");
+            displayView.append("_id - name - breed - gender - weight");
+            displayView.append("\n");
+            displayView.append("\n");
+
+            while(cursor.moveToNext()) {
+                displayView.append(cursor.getString(petID));
+                displayView.append(" - ");
+                displayView.append(cursor.getString(petName));
+                displayView.append(" - ");
+                displayView.append(cursor.getString(petBreed));
+                displayView.append(" - ");
+                displayView.append(cursor.getString(petGender));
+                displayView.append(" - ");
+                displayView.append(cursor.getString(petWeight));
+                displayView.append("\n");
+            }
         } finally {
             // Always close the cursor when you're done reading from it. This releases all its
             // resources and makes it invalid.
